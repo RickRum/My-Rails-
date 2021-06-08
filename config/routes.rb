894d8c601061f412cf2bc8939852b59eb92fd5f8
RickRum <rickrum@mail.ru>
2021-06-08 23:25:47 +0400

    faundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  devise_for :users
+  root "posts#index" , as: "home"
+  namespace :admin do 
+    get '/', to: 'posts#index'
+    resources :posts
+  end 
 end
